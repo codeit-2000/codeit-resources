2878f908943f991c5df9e888050cd5e117da0bff
easyhyun00 <leejihyun0324@gmail.com>
2024-10-11 01:06:47 +0900

    📦 chore: storybook 설치 및 적용

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+import { Linter } from 'eslint';
+
+/** @type {Linter.FlatConfig[]} */
+export default [
+  {
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    rules: {
+      // 여기에 필요한 규칙을 추가하세요.
+    },
+    // 추가 설정 가능
+  },
+];
