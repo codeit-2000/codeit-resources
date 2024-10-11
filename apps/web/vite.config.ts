81c6c2663afb389a54219c77818448fcfd512309
Daeyang lee <eodid007@gmail.com>
2024-10-11 17:58:25 +0900

    📦 chore: 절대경로 설정

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@src": resolve(__dirname, "src"), // 절대 경로 설정
+    },
+  },
+});
