f0e957d9431329406b499b9097fd82e753246928
Daeyang lee <eodid007@gmail.com>
2024-10-12 00:41:22 +0900

    💄 design: Sidebar 임시 스타일 적용

--- apps/web/src/components/layout/Sidebar/index.tsx
+++ apps/web/src/components/layout/Sidebar/index.tsx
@@ -1,8 +1,19 @@
 import { Link } from "react-router-dom";
+import { css } from "@emotion/react";
 
 export default function Sidebar() {
   return (
-    <nav>
+    <nav
+      css={css`
+        position: fixed;
+        left: 0;
+        top: 0;
+        width: 100px;
+        height: 100%;
+        background-color: #f0f0f0;
+        padding: 10px;
+      `}
+    >
       <ul>
         <Link to="/dashboard">
           <li>대시보드</li>
