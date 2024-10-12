58b8a92bd517c446e318b429eab75ad84662fc37
alexgoni <goni000211@gmail.com>
2024-10-12 23:05:19 +0900

    📦 chore: 초기 세팅

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { Amplify } from "aws-amplify";
+import outputs from "@repo/backend/outputs";
+import "@aws-amplify/ui-react/styles.css";
+import { Authenticator } from "@aws-amplify/ui-react";
 
-createRoot(document.getElementById('root')!).render(
+Amplify.configure(outputs);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <Authenticator.Provider>
+      <Authenticator>
+        <App />
+      </Authenticator>
+    </Authenticator.Provider>
   </StrictMode>,
-)
+);
