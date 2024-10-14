39c2b8b6a69a77618fff2440d605016cee3792af
DAEYANG LEE <108586797+oceanlee-seoul@users.noreply.github.com>
2024-10-14 17:46:28 +0900

    📦️ Chore(#20): SVGR 및 폰트 세팅 (#28)
    * 💄 design: emotion reset css 제거 및 index.css에 reset 추가
    
    * 🔥 remove: amplify style.css 제거
    
    * ✨ feat: svgr 세팅

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,13 +1,11 @@
 import "@src/App.css";
 import Layout from "./components/layout";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import ResetCss from "@src/styles/ResetCss";
 import AmplifyTest from "@src/pages/amplify-test";
 
 function App() {
   return (
     <>
-      <ResetCss />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Layout />} />
