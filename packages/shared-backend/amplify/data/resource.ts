43024d8b06c466662c8e761a7db1e5595d564d37
alexgoni <goni000211@gmail.com>
2024-10-16 11:30:36 +0900

    ✨ feat: 뼈대

--- packages/shared-backend/amplify/data/resource.ts
+++ packages/shared-backend/amplify/data/resource.ts
@@ -1,14 +1,33 @@
 import { a, defineData, type ClientSchema } from "@aws-amplify/backend";
 
 const schema = a.schema({
-  Todo: a
+  // User Table
+
+  // Resource Table
+  Resource: a
     .model({
-      content: a.string(),
+      resourceType: a.enum(["Room", "Seat", "Equipment"]),
+      resourceSubtype: a.string(),
+      name: a.string().required(),
+      date: a.date().required(),
+      participants: a.string().array(),
+
+      // belongsTo
+      // hasMany
+      // SecondaryIndexes
     })
+    .secondaryIndexes((index) => [index("resourceType")])
+    // ex) resourceType 기준으로 쿼리
+    // const { data, errors } = await client.models.Resource.listResourceByResourceType({
+    //   resourceType: 'Seat',
+
     .authorization((allow) => [allow.owner()]),
+
+  // Reservation Table
 });
 
 export type Schema = ClientSchema<typeof schema>;
+
 export const data = defineData({
   schema,
   authorizationModes: {
