4a8cf54e7d7865e60adbc2f6690d4f784b52ed73
DAEYANG LEE <108586797+oceanlee-seoul@users.noreply.github.com>
2024-10-19 17:10:29 +0900

    ✨ Feat(#6): Global Modal System  (#61)
    * 🐛 fix: 버튼 컴포넌트 사이즈 프롭 추가
    
    * ✨ feat: Modal 컴포넌트 추가
    
    * ✨ feat: 버튼 size 프롭 추가한 거 스토리북 반영
    
    * ✨ feat: 전역 모달 시스템 구현 및 스토리북 설정
    
    * 📦 chore: pnpm-lock 추가
    
    * ♻️ refactor: Modal 컴포넌트 이름 변경, 불필요한 코드 변경, 주석 추가
    
    * 🐛 fix: lint 반영
    
    * ✨ feat: Modal 애니메이션 추가
    
    * ♻️ refactor: Modal Refactoring

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter } from "react-router-dom";
 import MobileSizeWatcher from "./components/layout/MobileSizeWatcher";
+import ModalProvider from "./components/commons/Modal";
 import Router from "./Router";
 
 function App() {
   return (
     <BrowserRouter>
         <MobileSizeWatcher />
+        <ModalProvider />
         <Router />
       </BrowserRouter>
   );
---
+++ apps/web/src/components/commons/Modal/index.stories.tsx
@@ -0,0 +1,36 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import Button from "@src/components/commons/Button";
+import useModal from "@src/hooks/useModal";
+import Modal from ".";
+
+const meta = {
+  title: "Web Components/Modal",
+  components: Modal,
+  tags: ["autodocs"],
+} as Meta<typeof Modal>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const MoveSeatConfirmModal: Story = {
+  render: () => {
+    const { openModal } = useModal();
+
+    return (
+      <div>
+        <Button
+          width="w-200"
+          onClick={() => {
+            openModal("moveSeatConfirm", {
+              onConfirm: () => {},
+            });
+          }}
+        >
+          모달 열기
+        </Button>
+        <Modal />
+      </div>
+    );
+  },
+};
