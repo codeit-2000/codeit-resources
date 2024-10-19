b0378c50ef531e36066e44b5ea56416daf36421c
eprnf <eprnf21@gmail.com>
2024-10-19 22:54:50 +0900

    📦 chore: 절대 경로, expo svgr 설정

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -1,6 +1,22 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["./"],
+          alias: {
+            "@components": "/components",
+            "@navigation": "/navigation",
+            "@hooks": "/hooks",
+            "@screens": "/screens",
+            "@lib": "/lib",
+            "@assets": "/assets",
+          },
+        },
+      ],
+    ],
   };
 };
