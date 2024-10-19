f7c2e8cfbe86db31f453960af8663a74e0b85536
easyhyun00 <leejihyun0324@gmail.com>
2024-10-19 17:07:56 +0900

    ✨ feat: 이메일 자동 인증 구현

--- apps/web/src/pages/auth-test/index.tsx
+++ apps/web/src/pages/auth-test/index.tsx
@@ -1,18 +1,17 @@
 import { useState } from "react";
 import type { Schema } from "@repo/backend/amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
-import { signIn, signOut, signUp, confirmSignUp } from "aws-amplify/auth";
+import { signIn, signOut, signUp } from "aws-amplify/auth";
 import Button from "@src/components/commons/Button";
+import { autoAuthUser } from "@src/utils/autoAuthUser";
 
 const client = generateClient<Schema>();
 
 const AuthTestPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [code, setCode] = useState("");
   const [newEmail, setNewEmail] = useState("");
   const [name, setName] = useState("");
-  const [newPassword, setNewPassword] = useState("");
 
   // 로그인
   const handleLogin = async () => {
@@ -37,29 +36,18 @@ const AuthTestPage = () => {
     }
   };
 
-  // 이메일 인증
-  const handleConfirm = async () => {
-    try {
-      await confirmSignUp({
-        username: newEmail,
-        confirmationCode: code,
-      });
-      alert("인증 성공");
-    } catch (error) {
-      console.error("인증 실패", error);
-      alert("인증 실패");
-    }
-  };
-
   // 회원가입
   const handleSignUp = async () => {
     try {
       const result = await signUp({
         username: newEmail,
-        password: newPassword,
+        password: "Test12345!",
       });
-      console.log("회원가입 성공! 이메일 인증해주세요", result);
-      alert("회원가입 성공! 이메일 인증 해주세요");
+      console.log("회원가입 성공!", result);
+
+      autoAuthUser("MEMBER", newEmail);
+
+      alert("회원가입 성공!");
       client.models.User.create({
         id: result.userId,
         username: name,
@@ -95,7 +83,7 @@ const AuthTestPage = () => {
         </Button>
       </div>
       <div>
-        <h2 className="text-20-700">회원가입 하기</h2>
+        <h2 className="text-20-700">멤버 추가 하기</h2>
         <input
           placeholder="이름"
           value={name}
@@ -106,24 +94,9 @@ const AuthTestPage = () => {
           value={newEmail}
           onChange={(e) => setNewEmail(e.target.value)}
         />
-        <input
-          placeholder="비밀번호"
-          value={newPassword}
-          onChange={(e) => setNewPassword(e.target.value)}
-        />
+        <span>비밀번호는 Test12345! 입니다.</span>
         <Button width="w-120" onClick={handleSignUp}>
-          회원가입
-        </Button>
-      </div>
-      <div>
-        <h2 className="text-20-700">인증코드 입력</h2>
-        <input
-          placeholder="인증코드"
-          value={code}
-          onChange={(e) => setCode(e.target.value)}
-        />
-        <Button width="w-100" onClick={handleConfirm}>
-          인증
+          멤버추가
         </Button>
       </div>
     </div>
---
+++ apps/web/src/utils/autoAuthUser.ts
@@ -0,0 +1,78 @@
+import {
+  CognitoIdentityProviderClient,
+  AdminAddUserToGroupCommand,
+  AdminConfirmSignUpCommand,
+  AdminUpdateUserAttributesCommand,
+} from "@aws-sdk/client-cognito-identity-provider";
+
+const cognitoClient = new CognitoIdentityProviderClient({
+  region: "ap-northeast-2",
+  credentials: {
+    accessKeyId: import.meta.env.VITE_ACCESS_KEY_ID,
+    secretAccessKey: import.meta.env.VITE_SECRET_ACCESS_KEY,
+  },
+});
+
+// 유저를 특정 그룹에 추가
+const addUserToGroup = async (email: string, groupName: string) => {
+  const params = {
+    GroupName: groupName,
+    UserPoolId: import.meta.env.VITE_USER_POOL_ID,
+    Username: email,
+  };
+
+  try {
+    const command = new AdminAddUserToGroupCommand(params);
+    await cognitoClient.send(command);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// Cognito에서 바로 사용자 확인 (이메일 인증 없이 활성화)
+const confirmUserInCognito = async (email: string) => {
+  const adminConfirmParams = {
+    UserPoolId: import.meta.env.VITE_USER_POOL_ID,
+    Username: email,
+  };
+
+  try {
+    const command = new AdminConfirmSignUpCommand(adminConfirmParams);
+    await cognitoClient.send(command);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// 이메일 확인 여부를 강제로 "예"로 설정
+const verifyUserEmail = async (email: string) => {
+  const updateParams = {
+    UserPoolId: import.meta.env.VITE_USER_POOL_ID,
+    Username: email,
+    UserAttributes: [
+      {
+        Name: "email_verified",
+        Value: "true",
+      },
+    ],
+  };
+
+  try {
+    const command = new AdminUpdateUserAttributesCommand(updateParams);
+    await cognitoClient.send(command);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+/**
+ * @description 자동으로 유저를 그룹에 추가하고, 이메일 인증해줌
+ *
+ * @param groupName MEMBER | ADMIN
+ * @param email 유저 이메일
+ */
+export const autoAuthUser = (groupName: string, email: string) => {
+  addUserToGroup(email, groupName);
+  confirmUserInCognito(email);
+  verifyUserEmail(email);
+};
