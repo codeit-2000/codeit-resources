3986c58dfadd86d8acdb18201596f847391eaafb
alexgoni <goni000211@gmail.com>
2024-10-19 16:16:07 +0900

    ✨ feat: Profile Image 컴포넌트 구현

---
+++ apps/web/src/components/commons/ProfileImage/index.tsx
@@ -0,0 +1,26 @@
+import clsx from "clsx";
+import defaultProfile from "@repo/assets/images/default-profile.png";
+
+interface ProfileImagePros {
+  imageUrl?: string;
+  size?: "sm" | "md" | "lg";
+}
+
+export default function ProfileImage({
+  imageUrl,
+  size = "md",
+}: ProfileImagePros) {
+  const classnames = clsx("rounded-full object-cover", {
+    "size-32": size === "sm",
+    "size-40": size === "md",
+    "size-120": size === "lg",
+  });
+
+  return (
+    <img
+      src={imageUrl ? imageUrl : defaultProfile}
+      alt="profile"
+      className={classnames}
+    />
+  );
+}
