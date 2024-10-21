311060103302de635982414089911b696f388919
hoon <dudrn587@gmail.com>
2024-10-21 17:23:01 +0900

    ♻️ refactor: 불필요한 코드 삭제

--- apps/web/src/pages/meeting-rooms/index.tsx
+++ apps/web/src/pages/meeting-rooms/index.tsx
@@ -1,20 +1,6 @@
-import BottomSheet from "@src/components/commons/BottomSheet";
-import BottomSheetOpenButton from "@src/components/commons/BottomSheet/BottomSheetOpenButton";
-import { useState } from "react";
-
 export default function MeetingRooms() {
-  const [isBottomSheetOpen, setIsBottomSheetOpen] = useState(false);
-  const handleBottomSheetOpen = () => setIsBottomSheetOpen(true);
-
   return (
     <div>
-      <BottomSheetOpenButton handleBottomSheetOpen={handleBottomSheetOpen} />
-      {isBottomSheetOpen && (
-        <BottomSheet
-          isBottomSheetOpen={isBottomSheetOpen}
-          onClose={() => setIsBottomSheetOpen(false)}
-        />
-      )}
       <h1>This is Meeting Rooms Reservation Page :)</h1>
     </div>
   );
