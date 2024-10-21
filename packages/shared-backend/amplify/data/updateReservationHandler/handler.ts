dd02a33fbd1fa3f4516323f3c9b3cff40d2d24bc
eprnf <eprnf21@gmail.com>
2024-10-21 15:47:55 +0900

    ✨ feat: 수정,삭제,생성시 로직 추가

---
+++ packages/shared-backend/amplify/data/updateReservationHandler/handler.ts
@@ -0,0 +1,40 @@
+// createReservationHandler
+import { generateClient } from "aws-amplify/api";
+
+import type { Schema } from "../resource";
+
+const client = generateClient<Schema>();
+
+const userID = "234";
+
+export const handler: Schema["updateReservation"]["functionHandler"] = async (
+  event,
+  context,
+) => {
+  console.log("이벤트", event);
+  console.log("콘택스트", context);
+
+  const { id } = event.arguments;
+
+  // 1. 해당 예약 정보를 가져옴
+  const { data: reservation } = await client.models.Reservation.get({
+    id,
+  });
+
+  // 2. 참여자인지 확인
+  const isParticipants =
+    reservation &&
+    reservation.participants &&
+    reservation.participants.some(
+      (participant) => (participant as string) === userID,
+    );
+
+  if (!isParticipants) {
+    throw new Error("예약의 참여자만 수정/삭제가 가능합니다.");
+  }
+
+  // 3. 참여자면 업데이트
+  const { data } = await client.models.Reservation.update(event.arguments);
+
+  return data;
+};
