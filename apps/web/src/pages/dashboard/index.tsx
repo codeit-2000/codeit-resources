bfcf03d030d7103769f2eb7ad7abeada431f9a49
alexgoni <goni000211@gmail.com>
2024-10-21 17:52:54 +0900

    📝 docs: Add Story

--- apps/web/src/pages/dashboard/index.tsx
+++ apps/web/src/pages/dashboard/index.tsx
@@ -1,25 +1,7 @@
-import Tab from "@src/components/commons/Tab";
-
 export default function Dashboard() {
   return (
     <div>
       <h1>This is Dashboard Page :)</h1>
-
-      <Tab className="gap-12 bg-red-500">
-        {({ activeIndex, handleClick }) =>
-          Array.from({ length: 30 }).map((_, index) => (
-            <button
-              type="button"
-              // eslint-disable-next-line react/no-array-index-key
-              key={index}
-              className={`${index === activeIndex ? "text-blue-50" : ""}`}
-              onClick={() => handleClick(index)}
-            >
-              Item {index + 1}
-            </button>
-          ))
-        }
-      </Tab>
     </div>
   );
 }
