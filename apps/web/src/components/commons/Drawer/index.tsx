6d7f17d84e0fcd09e9ff38a6758326d23a9d8963
hoon <dudrn587@gmail.com>
2024-10-21 17:18:20 +0900

    ✨ feat: 테블릿, 데스크탑용 drawer 구현

--- apps/web/src/components/commons/Drawer/index.tsx
+++ apps/web/src/components/commons/Drawer/index.tsx
@@ -1,52 +1,31 @@
-// import Arrow from "@repo/assets/icons/icon-double-right-arrow.svg";
-// import Button from "@src/components/commons/Button";
-// import { motion } from "framer-motion";
-// interface DrawerProps {
-//   isOpen: boolean;
-//   onClose: () => void;
-// }
-// function Drawer({ isOpen, onClose }: DrawerProps) {
-//   return (
-//     <motion.div
-//       initial={{ x: "100%" }}
-//       animate={{ x: isOpen ? 0 : "100%" }}
-//       transition={{ type: "spring", stiffness: 300, damping: 30 }}
-//       className="fixed right-0 top-0 z-50 h-full w-80 bg-white shadow-lg"
-//     >
-//       <div className="p-4">
-//         <button type="button" onClick={onClose} className="mb-4">
-//           <Arrow />
-//         </button>
-//         <Button className="w-full">예약하기</Button>
-//       </div>
-//     </motion.div>
-//   );
-// }
-// export default Drawer;
-// apps/web/src/components/commons/Drawer/index.tsx
-// Import Arrow correctly or replace with an inline SVG
 import Arrow from "@repo/assets/icons/icon-double-right-arrow.svg?react";
 import Button from "@src/components/commons/Button";
 import { motion } from "framer-motion";
+import { ReactNode } from "react";
 
 interface DrawerProps {
   onClose: () => void;
+  children: ReactNode; // 드로워 안에 내용들 children으로 채워주기
 }
 
-function Drawer({ onClose }: DrawerProps) {
+function Drawer({ onClose, children }: DrawerProps) {
   return (
     <motion.div
       initial={{ x: "100%" }}
       animate={{ x: 0 }}
       exit={{ x: "100%" }}
       transition={{ type: "spring", stiffness: 240, damping: 30 }}
-      className="border-l-gray-40 fixed right-0 top-0 z-50 h-full w-1/3 border bg-white p-32 shadow-lg"
+      className="border-l-gray-40 fixed right-0 top-0 z-50 flex h-full w-1/3 flex-col border bg-white p-32 shadow-lg"
     >
-      <div>
-        <button type="button" onClick={onClose}>
+      <div className="flex items-center justify-center">
+        <button
+          type="button"
+          className="absolute left-16 top-16"
+          onClick={onClose}
+        >
           <Arrow />
         </button>
-
+        {children}
         <div className="fixed bottom-40 w-[90%]">
           <Button>예약하기</Button>
         </div>
