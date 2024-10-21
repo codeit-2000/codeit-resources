7ec2bd9501f31f0a901d1010d2ef007e6715605c
Jeongmin <154623483+oris8@users.noreply.github.com>
2024-10-21 17:50:42 +0900

    Revert "✨ Feat(#78): 예약 수정,삭제,생성시 로직 추가 (#79)"
    This reverts commit e16919309c88e306b015c47fdf28c4c29144bab9.

--- apps/web/src/components/layout/NavigationBar/NavOptionList.tsx
+++ apps/web/src/components/layout/NavigationBar/NavOptionList.tsx
@@ -1,7 +1,7 @@
-import Equipment from "@repo/assets/icons/icon-equipment.svg?react";
-import Meeting from "@repo/assets/icons/icon-meeting.svg?react";
 import Person from "@repo/assets/icons/icon-person.svg?react";
+import Meeting from "@repo/assets/icons/icon-meeting.svg?react";
 import Seats from "@repo/assets/icons/icon-seats.svg?react";
+import Equipment from "@repo/assets/icons/icon-equipment.svg?react";
 
 export interface NavOptionProps {
   id: string;
@@ -13,25 +13,25 @@ export interface NavOptionProps {
 const NAV_OPTION_LIST: NavOptionProps[] = [
   {
     id: "dashboard",
-    imgSrc: ({ stroke }) => <Person color={stroke} />,
+    imgSrc: ({ stroke }) => <Person stroke={stroke} />,
     text: "대시보드",
     path: "/dashboard",
   },
   {
     id: "meeting-rooms",
-    imgSrc: ({ stroke }) => <Meeting color={stroke} />,
+    imgSrc: ({ stroke }) => <Meeting stroke={stroke} />,
     text: "회의실",
     path: "/meeting-rooms",
   },
   {
     id: "seats",
-    imgSrc: ({ stroke }) => <Seats color={stroke} />,
+    imgSrc: ({ stroke }) => <Seats stroke={stroke} />,
     text: "좌석",
     path: "/seats",
   },
   {
     id: "equipments",
-    imgSrc: ({ stroke }) => <Equipment color={stroke} />,
+    imgSrc: ({ stroke }) => <Equipment stroke={stroke} />,
     text: "장비",
     path: "/equipments",
   },
@@ -39,25 +39,25 @@ const NAV_OPTION_LIST: NavOptionProps[] = [
    * 어드민 role 추가되면 추가할 예정입니다
   {
     id: "admin-members",
-    imgSrc: ({ stroke }) => <Person color={stroke} />,
+    imgSrc: ({ stroke }) => <Person stroke={stroke} />,
     text: "멤버 관리",
     path: "/admin/members",
   },
   {
     id: "admin-meeting-rooms",
-    imgSrc: ({ stroke }) => <Meeting color={stroke} />,
+    imgSrc: ({ stroke }) => <Meeting stroke={stroke} />,
     text: "회의실 설정",
     path: "/admin/meeting-rooms",
   },
   {
     id: "admin-seats",
-    imgSrc: ({ stroke }) => <Seats color={stroke} />,
+    imgSrc: ({ stroke }) => <Seats stroke={stroke} />,
     text: "좌석 설정",
     path: "/admin/seats",
   },
   {
     id: "admin-equipments",
-    imgSrc: ({ stroke }) => <Equipment color={stroke} />,
+    imgSrc: ({ stroke }) => <Equipment stroke={stroke} />,
     text: "장비 설정",
     path: "/admin/equipments",
   },
