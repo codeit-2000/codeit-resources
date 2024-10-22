660596ec20d11469d582924a6a72bcef01fbb916
hoon <dudrn587@gmail.com>
2024-10-22 13:52:47 +0900

    💄 design: 데스크탑 드로워 스토리 추가

---
+++ apps/web/src/components/commons/Drawer/index.stories.tsx
@@ -0,0 +1,70 @@
+import Button from "@src/components/commons/Button";
+import Drawer from "@src/components/commons/Drawer";
+import type { Meta, StoryObj } from "@storybook/react";
+import { AnimatePresence } from "framer-motion";
+import { ReactNode, useState } from "react";
+
+const meta = {
+  title: "Components/Common/Drawer", // 계층 구조를 더 명확하게 표현
+  component: Drawer,
+  tags: ["autodocs"],
+  parameters: {
+    layout: "centered",
+    docs: {
+      description: {
+        component:
+          "슬라이딩 효과가 있는 드로워 컴포넌트입니다. 오른쪽에서 왼쪽으로 열립니다.",
+      },
+    },
+  },
+} satisfies Meta<typeof Drawer>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+interface DrawerStoryProps {
+  buttonText: string;
+  children: ReactNode;
+}
+
+function DrawerStory({ buttonText, children }: DrawerStoryProps) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div>
+      <button
+        type="button"
+        onClick={() => setIsOpen(true)}
+        className="rounded bg-blue-500 px-16 py-8 text-white"
+      >
+        드로워 열기
+      </button>
+      <AnimatePresence>
+        {isOpen && (
+          <Drawer onClose={() => setIsOpen(false)} buttonText={buttonText}>
+            {children}
+            <div className="fixed bottom-40 w-[90%]">
+              <Button>{buttonText}</Button>
+            </div>
+          </Drawer>
+        )}
+      </AnimatePresence>
+    </div>
+  );
+}
+
+// 기본 Drawer 스토리
+export const Default: Story = {
+  args: {
+    buttonText: "저장하기",
+    onClose: () => {},
+    children: null,
+  },
+  render: (args) => (
+    <DrawerStory buttonText={args.buttonText}>
+      <div className="items-start">
+        <p className="mb-8">드로워에 들어갈 요소들</p>
+      </div>
+    </DrawerStory>
+  ),
+};
