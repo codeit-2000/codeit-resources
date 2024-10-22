ace07beb87535030ef1a7a368d48c2f43cd29bae
alexgoni <goni000211@gmail.com>
2024-10-22 13:47:47 +0900

    📦 chore: 기본 세팅

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,18 +1,25 @@
 import ToastProvider from "@src/components/commons/Toast/ToastProvider";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { BrowserRouter } from "react-router-dom";
 
 import Router from "./Router";
 import ModalProvider from "./components/commons/Modal";
 import MobileSizeWatcher from "./components/layout/MobileSizeWatcher";
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <BrowserRouter>
-      <ToastProvider />
-      <MobileSizeWatcher />
-      <ModalProvider />
-      <Router />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <ToastProvider />
+        <MobileSizeWatcher />
+        <ModalProvider />
+        <Router />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
