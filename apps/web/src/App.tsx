c3dcb306af3453d055534d36ee6db109aed1f3ac
alexgoni <goni000211@gmail.com>
2024-10-22 15:47:45 +0900

    📦 Chore: Tanstack Query 기본 세팅 (#92)
    * 📦 chore: 기본 세팅
    
    * 📦 chore: 기본 설정 추가
    
    * 🐛 fix: 타입 에러 수정

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,18 +1,32 @@
 import ToastProvider from "@src/components/commons/Toast/ToastProvider";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { BrowserRouter } from "react-router-dom";
 
 import Router from "./Router";
 import ModalProvider from "./components/commons/Modal";
 import MobileSizeWatcher from "./components/layout/MobileSizeWatcher";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      staleTime: 60000,
+    },
+  },
+});
+
 function App() {
   return (
-    <BrowserRouter>
-      <ToastProvider />
-      <MobileSizeWatcher />
-      <ModalProvider />
-      <Router />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <ToastProvider />
+        <MobileSizeWatcher />
+        <ModalProvider />
+        <Router />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
