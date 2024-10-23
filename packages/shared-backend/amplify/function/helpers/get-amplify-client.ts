55c04c932a51266dc76d0aeb605c925633ba4d19
eprnf <eprnf21@gmail.com>
2024-10-23 21:42:09 +0900

    ✅ test: tsconfig

--- packages/shared-backend/amplify/function/helpers/get-amplify-client.ts
+++ packages/shared-backend/amplify/function/helpers/get-amplify-client.ts
@@ -3,12 +3,12 @@ import { Amplify } from "aws-amplify";
 import { generateClient } from "aws-amplify/data";
 import dotenv from "dotenv";
 
-// import config from "../../../amplify_outputs.json";
+import config from "../../../amplify_outputs.json";
 import { Schema } from "../../data/resource";
 import atest from "./test.json";
 
-// dotenv.config();
-// Amplify.configure(config);
+dotenv.config();
+Amplify.configure(config);
 
 console.log(atest);
 // export const client = generateClient<Schema>({
