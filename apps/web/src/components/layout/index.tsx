44cbef3309f4ecc00fbb28bd6c03de85bc22681f
이지현 <leejihyun0324@gmail.com>
2024-10-23 15:00:02 +0900

    ✨ Feat(#85): 로그인 및 ADMIN 여부에 따른 페이지 렌더링 (#99)
    * ✨ feat: user 관련 atom 생성
    
    * ✨ feat: 로그인 여부에 따른 페이지 접근 제한 구현
    
    * ✨ feat: admin 페이지 접근 제한 구현
    
    * ♻️ refactor: lint 오류
    
    * ✨ feat: 절대경로로 변경

--- apps/web/src/components/layout/index.tsx
+++ apps/web/src/components/layout/index.tsx
@@ -1,7 +1,49 @@
+import type { Schema } from "@repo/backend/amplify/data/resource";
 import NavigationBar from "@src/components/layout/NavigationBar";
-import { Outlet } from "react-router-dom";
+import { adminAtom, authAtom, userAtom } from "@src/store/authUserAtom";
+import { getCurrentUser } from "aws-amplify/auth";
+import { generateClient } from "aws-amplify/data";
+import { useAtom, useSetAtom } from "jotai";
+import { useEffect } from "react";
+import { Navigate, Outlet } from "react-router-dom";
+
+const client = generateClient<Schema>();
 
 export default function Layout() {
+  const [isAuthenticated, setIsAuthenticated] = useAtom(authAtom);
+  const setUser = useSetAtom(userAtom);
+  const setIsAdmin = useSetAtom(adminAtom);
+
+  useEffect(() => {
+    const getUser = async () => {
+      try {
+        const currentUser = await getCurrentUser();
+        if (currentUser) {
+          const userResponse = await client.models.User.get({
+            id: currentUser.userId,
+          });
+          const userData = userResponse?.data;
+
+          if (userData) {
+            setUser(userData);
+            setIsAdmin(userData.role === "ADMIN");
+            setIsAuthenticated(true);
+          } else {
+            setIsAuthenticated(false);
+          }
+        } else {
+          setIsAuthenticated(false);
+        }
+      } catch (error) {
+        setIsAuthenticated(false);
+      }
+    };
+    getUser();
+  }, [setUser, setIsAuthenticated]);
+
+  if (isAuthenticated === false) {
+    return <Navigate to="/sign-in" />;
+  }
 
   return (
     <>
