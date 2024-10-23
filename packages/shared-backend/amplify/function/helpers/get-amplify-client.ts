f47ea97f2b4871f945425fade4084e20cbf33e78
eprnf <eprnf21@gmail.com>
2024-10-23 21:34:05 +0900

    ✅ test: test

--- packages/shared-backend/amplify/function/helpers/get-amplify-client.ts
+++ packages/shared-backend/amplify/function/helpers/get-amplify-client.ts
@@ -1,14 +1,18 @@
-// import { Amplify } from "aws-amplify";
-// import { generateClient } from "aws-amplify/data";
-// import dotenv from "dotenv";
+// @ts-ignore
+import { Amplify } from "aws-amplify";
+import { generateClient } from "aws-amplify/data";
+import dotenv from "dotenv";
 
 // import config from "../../../amplify_outputs.json";
-// import { Schema } from "../../data/resource";
+import { Schema } from "../../data/resource";
+import atest from "./test.json";
 
 // dotenv.config();
 // Amplify.configure(config);
-// // export const client = generateClient<Schema>({
-// //   authMode: "lambda",
-// //   authToken: process.env.ADMIN_API_KEY,
-// // });
-// export const client = generateClient<Schema>();
+
+console.log(atest);
+// export const client = generateClient<Schema>({
+//   authMode: "lambda",
+//   authToken: process.env.ADMIN_API_KEY,
+// });
+export const client = generateClient<Schema>();
