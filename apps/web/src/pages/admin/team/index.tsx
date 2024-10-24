524c7c34f065ba6f7f98fc4f4a3355c16eb4ce8e
이지현 <leejihyun0324@gmail.com>
2024-10-24 13:53:34 +0900

    ✨ Feat(#105): 팀 관리 페이지 팀 추가 UI 및 기능 구현 (#106)
    * ✨ feat: 팀 추가 모달 생성
    
    * ✨ feat: 팀 관리 페이지 상단 부분 구현
    
    * ♻️ refactor: team 테이블 key를 name으로 설정
    
    * ♻️ refactor: team 스키마 복귀
    
    * ✨ feat: 중복 팀 확인 구현

--- apps/web/src/pages/admin/team/index.tsx
+++ apps/web/src/pages/admin/team/index.tsx
@@ -1,5 +1,29 @@
+import Button from "@src/components/commons/Button";
+import useModal from "@src/hooks/useModal";
+
+import TeamList from "./components/TeamList";
+
 function AdminTeamPage() {
-  return <div>Admin만 접근할 수 있는 페이지입니다.</div>;
+  const { openModal } = useModal();
+
+  return (
+    <div className="bg-gray-5 md:px-88 lg:px-118 px-58 h-screen pt-80">
+      <div className="flex items-center justify-between">
+        <h1 className="text-24-700 md:text-28-700 text-gray-100">팀 관리</h1>
+        <Button
+          onClick={() => {
+            openModal("addTeamModal");
+          }}
+          variant="secondary"
+          width="w-110"
+          height="h-42"
+        >
+          + 팀 추가
+        </Button>
+      </div>
+      <TeamList />
+    </div>
+  );
 }
 
 export default AdminTeamPage;
